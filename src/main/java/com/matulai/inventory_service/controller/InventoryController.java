b2e6645615889a0c081f56568ac8a5a759ca1d70
Matias Laime <matulai1221@gmail.com>
2025-11-17 12:14:00 -0300

    Add: inventory controller

---
+++ src/main/java/com/matulai/inventory_service/controller/InventoryController.java
@@ -0,0 +1,21 @@
+package com.matulai.inventory_service.controller;
+
+import com.matulai.inventory_service.service.InventoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+public class InventoryController {
+    private final InventoryService inventoryService;
+
+    public InventoryController(InventoryService inventoryService) {
+        this.inventoryService = inventoryService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity) {
+        return inventoryService.isInStock(skuCode, quantity);
+    }
+}
