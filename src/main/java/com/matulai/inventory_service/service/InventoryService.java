cf837e1079e2284a32584ce2e3e706a690c68c8a
Matias Laime <matulai1221@gmail.com>
2025-11-17 12:19:14 -0300

    Add: inventory service

---
+++ src/main/java/com/matulai/inventory_service/service/InventoryService.java
@@ -0,0 +1,17 @@
+package com.matulai.inventory_service.service;
+
+import com.matulai.inventory_service.repository.InventoryRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InventoryService {
+    private final InventoryRepository inventoryRepository;
+
+    public InventoryService(InventoryRepository inventoryRepository) {
+        this.inventoryRepository = inventoryRepository;
+    }
+
+    public boolean isInStock(String skuCode, Integer quantity) {
+        return inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode, quantity);
+    }
+}
